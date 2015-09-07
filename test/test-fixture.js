cfe2d9d670ead93860a02bd24c1e9a88f0f458a6
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-07 13:47:56 +0300

    add test fixture

---
+++ test/test-fixture.js
@@ -0,0 +1,26 @@
+describe('Suite 1', function() {
+  before(function() {
+  });
+
+  beforeEach(function() {
+  });
+
+  it('Test 1 in Suite 1', function() {
+  });
+
+  describe('Suite 1.1', function() {
+    before(function() {
+    });
+
+    it('Test 1 in Suite 1.1', function() {
+    });
+  });
+
+  it('Test 2 in Suite 1', function() {
+  });
+});
+
+describe('Suite 2', function() {
+  it('Test 1 in Suite 2', function() {
+  });
+});

81a4567b1568fa6dbd526236c0517414f145af15
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-10 15:54:01 +0300

    add one global test to the test structure fixture

---
+++ test/fixtures/test-structure.js
@@ -0,0 +1,21 @@
+describe('Suite 1', function() {
+  it('Test 1 in Suite 1', function() {
+  });
+
+  describe('Suite 1.1', function() {
+    it('Test 1 in Suite 1.1', function() {
+    });
+  });
+
+  it('Test 2 in Suite 1', function() {
+    throw new Error('error');
+  });
+});
+
+describe('Suite 2', function() {
+  it('Test 1 in Suite 2', function() {
+  });
+});
+
+it('Test 1 global', function() {
+});

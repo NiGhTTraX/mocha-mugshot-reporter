1f964e2629e0430562a1ef1ee167c8fa9a488c91
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-14 14:52:39 +0300

    Finish file generator #4

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var reporters = require('mocha').reporters;
+var reporters = require('mocha').reporters,
+    generate = require('./lib/data-generator.js'),
+    fs = require('fs'),
+    path = require('path');
+
+/**
+ * The directory of the visual report
+ *
+ * @type {String}
+ * @const
+ * @default
+ */
+var rootDirectory = 'visual-report';
+
+/**
+ * The path of the data file
+ *
+ * @type {String}
+ * @const
+ * @default
+ */
+var dataPath = 'data.js';
 
 /**
  * Mocha reporter for Mugshot visual regression testing lib
@@ -20,6 +41,30 @@ function MugshotReporter(runner, options) {
   }
 
   new CLIReporter(runner);
+
+  try {
+    fs.mkdirSync(rootDirectory);
+  } catch(error) {
+    if (error.code !== 'EEXIST') {
+      throw error;
+    }
+  }
+
+  generate(runner, function(data) {
+    this.data = data;
+  }.bind(this));
 };
 
 module.exports = MugshotReporter;
+
+MugshotReporter.prototype.done = function(failures, fn) {
+  var output = 'var data = ' + JSON.stringify(this.data) + ';';
+
+  fs.writeFile(path.join(rootDirectory, dataPath), output, function(error) {
+    if (error) {
+      throw error;
+    }
+
+    fn(failures);
+  });
+}

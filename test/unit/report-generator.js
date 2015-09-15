a95e8fb78b24f26176ca9ac5c126aabce720eeaf
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-15 12:04:42 +0300

    Update tests from integration to unit #4

--- test/unit/report-generator.js
+++ test/unit/report-generator.js
@@ -1,29 +1,13 @@
 var expect = require('chai').expect,
-    Mocha = require('mocha'),
+    generateReport = require('../../lib/report-generator.js'),
     fs = require('fs'),
     path = require('path');
 
-var testFixturePath = path.join(__dirname, '../fixtures/test-structure.js'),
-    mugshotReporter = path.join(__dirname, '../../index.js'),
-    rootDirectory = './visual-report',
+var rootDirectory = './visual-report',
     dataPath = path.join(rootDirectory, 'data.js'),
-    expectedData = require('./data/test-structure-data.js'),
-    mocha = new Mocha();
+    expectedData = require('./data/test-structure-data.js');
 
-mocha.addFile(testFixturePath);
-
-function runMocha(done) {
-  var stdoutWrite = process.stdout.write;
-
-  process.stdout.write = function() {};
-
-  mocha.reporter(mugshotReporter).run(function() {
-    process.stdout.write = stdoutWrite;
-    done();
-  });
-}
-
-describe('File generator', function() {
+describe('Report generator', function() {
 
   afterEach(function() {
     try {
@@ -43,8 +27,18 @@ describe('File generator', function() {
     }
   });
 
+  it('should throw Error if no data is received', function() {
+    expect(generateReport.bind(null)).to.throw(Error);
+  });
+
+  it('should throw Error if no callback is received', function() {
+    expect(generateReport.bind(null, {})).to.throw(Error);
+  });
+
   it('should create the rootDirectory', function(done) {
-    runMocha(function() {
+    generateReport({}, function(error) {
+      expect(error).to.be.undefined;
+
       var stats = fs.statSync(rootDirectory);
 
       expect(stats.isDirectory()).to.be.true;
@@ -54,7 +48,9 @@ describe('File generator', function() {
   });
 
   it('should create a file named "data.js"', function(done) {
-    runMocha(function() {
+    generateReport({}, function(error) {
+      expect(error).to.be.undefined;
+
       var stats = fs.statSync(dataPath);
 
       expect(stats.isFile()).to.be.true;
@@ -64,7 +60,9 @@ describe('File generator', function() {
   });
 
   it('should contain the data of the test-structure fixture', function(done) {
-    runMocha(function() {
+    generateReport(expectedData, function(error) {
+      expect(error).to.be.undefined;
+
       var expected = 'var data = ' + JSON.stringify(expectedData) + ';';
 
       fs.readFile(dataPath, 'utf8', function(error, data) {

a77d355324191c11a2abfb35a6d912a6a4cad34f
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-24 17:21:59 +0300

    Unify regexps in webpack config #10

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader'},
-      {test: /\.jsx$/, loader: 'babel-loader'},
+      {test: /\.jsx?$/, loader: 'babel-loader'},
       {test: /\.css/, loader: 'style-loader!css-loader'}
     ]
   }

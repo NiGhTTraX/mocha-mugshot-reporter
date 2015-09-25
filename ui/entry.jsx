283b488f12f73ab279026dcdd571560e8341fbbe
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-25 15:53:13 +0300

    Update root comp to mount directy in the body #9

--- ui/entry.jsx
+++ ui/entry.jsx
@@ -8,4 +8,4 @@ require('./styles/base.css');
 
 var data = window[mochaTestDataVarName];
 
-React.render(<Report data={data}/>, document.getElementById('report'));
+React.render(<Report data={data}/>, document.body);

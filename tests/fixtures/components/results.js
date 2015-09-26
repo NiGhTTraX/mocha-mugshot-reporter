cfb60d6af9e2de07af61cfe0c8e6370b6717f103
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-26 15:07:55 +0300

    Add fixture for Restuls comp #12

---
+++ tests/fixtures/components/results.js
@@ -0,0 +1,26 @@
+module.exports = [{
+  title: '',
+  indent: 0,
+  tests: [{
+    title: 'Test global',
+    state: 'passed',
+    duration: 155
+  }]
+}, {
+  title: 'Suite 1',
+  indent: 1,
+  tests: [{
+    title: 'Test 1',
+    state: 'failed',
+    error: new Error('Big Error'),
+    duration: 7
+  }, {
+    title: 'Test 2',
+    state: 'passed',
+    duration: 1.5
+  }]
+}, {
+  title: 'Suite 2',
+  indent: 1,
+  tests: []
+}];

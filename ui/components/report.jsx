ac64c7be8d6ada12a581ed1158e14caaa70c3e0e
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-28 13:20:28 +0300

    Use modern for: forEach and filter #9

--- ui/components/report.jsx
+++ ui/components/report.jsx
@@ -3,25 +3,28 @@ var React = require('react'),
 
 var Report = React.createClass({
   render: function() {
-    var data = this.props.data,
-        passes = 0,
-        failures = 0,
+    var suites = this.props.data,
+        passes = [],
+        failures = [],
         duration = 0;
 
-    for (var i = 0; i < data.length; i++) {
-      for (var j = 0; j < data[i].tests.length; j++) {
-        if (data[i].tests[j].state === 'passed') {
-          passes++;
-        } else {
-          failures++;
-        }
+  suites.forEach(function(suite) {
+    passes.push.apply(passes, suite.tests.filter(function(test) {
+      return test.state === 'passed';
+    }));
 
-        duration += data[i].tests[j].duration;
-      }
-    }
+    failures.push.apply(failures, suite.tests.filter(function(test) {
+      return test.state !== 'passed';
+    }));
 
-    return <div className="report">
-      <Header passes={passes} failures={failures} duration={duration}/>
+    suite.tests.forEach(function(test) {
+        duration += test.duration;
+    });
+  });
+
+  return <div className="report">
+      <Header passes={passes.length} failures={failures.length}
+        duration={duration}/>
     </div>;
   }
 });

d8c7b3669c15f3f2796bcb4ebede4bf2c3f301ae
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-28 16:07:17 +0300

    Update description of global React #9

--- tests/unit/components/setup.js
+++ tests/unit/components/setup.js
@@ -1,10 +1,6 @@
 /**
- * The tests for our components need React, because we are using the new
- * technology of shallow rendering, so we are passing to the renderer
- * a component in jsx format, which will be of course replaced by Babel with
- * React.createElement(...).
- *
- * @see https://facebook.github.io/react/docs/test-utils.html#shallow-rendering
+ * The tests for our components need React, because we are using JSX to render
+ * them, which of course will be replaced by Babel with React.createElement(..).
  *
  * If React is not defined global or is not included in every test suite,
  * every test will fail, because React is undefined.

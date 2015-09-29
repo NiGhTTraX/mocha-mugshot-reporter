c3ed0245bdaabda3c70e98c7c2bfb86b1fab3c00
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-29 18:22:00 +0300

    Implement Test comp #14

--- ui/components/test.jsx
+++ ui/components/test.jsx
@@ -1,8 +1,27 @@
-var React = require('react');
+var React = require('react/addons'),
+    Details = require('./details.jsx');
 
 var Test = React.createClass({
+  getInitialState: function() {
+    return {toggle: false};
+  },
+  displayDetails: function() {
+    this.setState({toggle: !this.state.toggle});
+  },
   render: function() {
-    return null;
+    var test = this.props.test,
+        cx = React.addons.classSet,
+        classes = cx({
+          test: true,
+          toogle: this.state.toggle
+        });
+
+    return <div className={classes}>
+      <p onClick={this.displayDetails}>{test.title}
+        <span>{test.state}</span>
+      </p>
+      <Details/>
+    </div>;
   }
 });
 

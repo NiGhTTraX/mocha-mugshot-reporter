a621f9c338e4afb5d902303716ab77fe1d616dfc
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-30 17:28:14 +0300

    Put baseline directly in the array declaration #17

--- ui/components/details.jsx
+++ ui/components/details.jsx
@@ -3,10 +3,8 @@ var React = require('react');
 var Details = React.createClass({
   render: function() {
     var paths = this.props.paths,
-        imgs = [];
-
-    imgs.push(<img className='baseline' src={paths.baseline}
-      key={paths.baseline}/>);
+        imgs = [<img className='baseline' src={paths.baseline}
+            key={paths.baseline}/>];
 
     if (Object.keys(paths).length > 1) {
       imgs.push(<img className='diff' src={paths.diff} key={paths.diff}/>);

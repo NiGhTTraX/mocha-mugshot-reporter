2bb23e851da6853efd67e2a6deab15e71ef53c65
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-30 17:02:27 +0300

    Refactor Details comp tests #17

--- tests/unit/components/details.js
+++ tests/unit/components/details.js
@@ -13,47 +13,54 @@ describe('Details', function() {
     expect(tree.findNode('.baseline')).to.not.be.false;
   });
 
-  it('should render the diff if there is one', function() {
-    tree = sd.shallowRender(<Details paths={failDetails}/>);
+  describe('Pass details', function() {
+    it('should not render the diff', function() {
+      tree = sd.shallowRender(<Details paths={passDetails}/>);
 
-    expect(tree.findNode('.diff')).to.not.be.false;
+      expect(tree.findNode('.diff')).to.be.false;
+    });
+
+    it('should not render the screenshot', function() {
+      tree = sd.shallowRender(<Details paths={passDetails}/>);
+
+      expect(tree.findNode('.screenshot')).to.be.false;
+    });
+
   });
 
-  it('should not render the diff if there is none', function() {
-    tree = sd.shallowRender(<Details paths={passDetails}/>);
+  describe('Fail details', function() {
+    it('should render the diff', function() {
+      tree = sd.shallowRender(<Details paths={failDetails}/>);
 
-    expect(tree.findNode('.diff')).to.be.false;
+      expect(tree.findNode('.diff')).to.not.be.false;
+    });
+
+    it('should render the screenshot', function() {
+      tree = sd.shallowRender(<Details paths={failDetails}/>);
+
+      expect(tree.findNode('.screenshot')).to.not.be.false;
+    });
   });
 
-  it('should render the screenshot if there is one', function() {
-    tree = sd.shallowRender(<Details paths={failDetails}/>);
+  describe('Paths', function() {
+    it('should have the correct baseline path', function() {
+      tree = sd.shallowRender(<Details paths={failDetails}/>);
 
-    expect(tree.findNode('.screenshot')).to.not.be.false;
-  });
+      expect(tree.findNode('.baseline').props.src).to.be.
+        equal(failDetails.baseline);
+    });
 
-  it('should not render the screenshot if there is none', function() {
-    tree = sd.shallowRender(<Details paths={passDetails}/>);
+    it('should have the correct screenshot path', function() {
+      tree = sd.shallowRender(<Details paths={failDetails}/>);
 
-    expect(tree.findNode('.screenshot')).to.be.false;
-  });
+      expect(tree.findNode('.screenshot').props.src).to.be.
+        equal(failDetails.screenshot);
+    });
 
-  it('should have the correct baseline path', function() {
-    tree = sd.shallowRender(<Details paths={failDetails}/>);
+    it('should have the correct diff path', function() {
+      tree = sd.shallowRender(<Details paths={failDetails}/>);
 
-    expect(tree.findNode('.baseline').props.src).to.be.
-      equal(failDetails.baseline);
-  });
-
-  it('should have the correct screenshot path', function() {
-    tree = sd.shallowRender(<Details paths={failDetails}/>);
-
-    expect(tree.findNode('.screenshot').props.src).to.be.
-      equal(failDetails.screenshot);
-  });
-
-  it('should have the correct diff path', function() {
-    tree = sd.shallowRender(<Details paths={failDetails}/>);
-
-    expect(tree.findNode('.diff').props.src).to.be.equal(failDetails.diff);
+      expect(tree.findNode('.diff').props.src).to.be.equal(failDetails.diff);
+    });
   });
 });

fec885ec314fa2a66983f2e37afd3d65984dafa5
Simion Florentin <florentin.simion@hootsuite.com>
2015-09-30 18:13:31 +0300

    Put duplicate code in forEach #17

--- tests/unit/components/details.js
+++ tests/unit/components/details.js
@@ -8,55 +8,47 @@ describe('Details', function() {
   var tree;
 
   describe('Passing', function() {
-    it('should render the baseline', function() {
+    beforeEach(function() {
       tree = sd.shallowRender(<Details paths={passDetails}/>);
+    });
 
+    it('should render the baseline', function() {
       expect(tree.findNode('.baseline')).to.not.be.false;
     });
 
     it('should not render the diff', function() {
-      tree = sd.shallowRender(<Details paths={passDetails}/>);
-
       expect(tree.findNode('.diff')).to.be.false;
     });
 
     it('should not render the screenshot', function() {
-      tree = sd.shallowRender(<Details paths={passDetails}/>);
-
       expect(tree.findNode('.screenshot')).to.be.false;
     });
 
     it('should have the correct baseline path', function() {
-      tree = sd.shallowRender(<Details paths={failDetails}/>);
-
       expect(tree.findNode('.baseline').props.src).to.be.
         equal(failDetails.baseline);
     });
   });
 
   describe('Fail details', function() {
-    it('should render the diff', function() {
+    beforeEach(function() {
       tree = sd.shallowRender(<Details paths={failDetails}/>);
+    });
 
+    it('should render the diff', function() {
       expect(tree.findNode('.diff')).to.not.be.false;
     });
 
     it('should render the screenshot', function() {
-      tree = sd.shallowRender(<Details paths={failDetails}/>);
-
       expect(tree.findNode('.screenshot')).to.not.be.false;
     });
 
     it('should have the correct screenshot path', function() {
-      tree = sd.shallowRender(<Details paths={failDetails}/>);
-
       expect(tree.findNode('.screenshot').props.src).to.be.
         equal(failDetails.screenshot);
     });
 
     it('should have the correct diff path', function() {
-      tree = sd.shallowRender(<Details paths={failDetails}/>);
-
       expect(tree.findNode('.diff').props.src).to.be.equal(failDetails.diff);
     });
   });

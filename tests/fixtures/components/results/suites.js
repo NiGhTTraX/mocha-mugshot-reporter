5b8c93e5c66e317fceb047f738b5296f50240e96
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-08-19 16:19:39 +0300

    Test Results component #23

---
+++ tests/fixtures/components/results/suites.js
@@ -0,0 +1,63 @@
+import _ from 'lodash';
+
+let passedTestDuration = _.random(0, 100, true);
+let failedTestDuration = _.random(0, 100, true);
+
+module.exports = {
+  suites: [{}, {
+    title: 'Suite 1',
+    indent: _.random(0, 100),
+    tests: [{
+      title: 'Test 1',
+      state: 'failed',
+      error: {
+        name: 'AssertionError',
+        message: 'expected baseline and screenshot of ' +
+            '{ name: \'baslinePath\' } to be identical'
+      },
+      result: {
+        isEqual: false,
+        baseline: 'baslinePath',
+        screenshot: 'screenshotPath',
+        diff: 'diffPath'
+      },
+      duration: failedTestDuration
+    }, {
+      title: 'Test 2',
+      state: 'passed',
+      result: {
+        isEqual: true,
+        baseline: 'baselinePath'
+      },
+      duration: passedTestDuration
+    }],
+    passes: [{
+      title: 'Test 2',
+      state: 'passed',
+      result: {
+        isEqual: true,
+        baseline: 'baselinePath'
+      },
+      duration: passedTestDuration
+    }],
+    failures: [{
+      title: 'Test 1',
+      state: 'failed',
+      error: {},
+      result: {
+        isEqual: false,
+        baseline: 'baslinePath',
+        screenshot: 'screenshotPath',
+        diff: 'diffPath'
+      },
+      duration: failedTestDuration
+    }]
+  }, {
+    title: 'Suite 2',
+    indent: _.random(0, 100),
+    tests: [],
+    passes: [],
+    failures: []
+  }],
+  filter: 'all'
+};

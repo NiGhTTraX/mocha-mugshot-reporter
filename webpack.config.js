b73f0571e0801f121f9ecca1b41df4060d490d84
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-08-22 17:17:19 +0300

    Bundle all styles into one file #25

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './ui/entry.jsx',
@@ -8,11 +9,16 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/},
-      {test: /\.css/, loader: 'style-loader!css-loader!postcss'},
-      {test: /\.less$/, loader: 'style!css!postcss!less'}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader',
+        'css-loader!postcss')},
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('style',
+        'css!postcss!less')}
     ]
   },
   postcss: [
     autoprefixer({browsers: ['last 2 versions']})
+  ],
+  plugins: [
+    new ExtractTextPlugin('./statics/style.css')
   ]
 };

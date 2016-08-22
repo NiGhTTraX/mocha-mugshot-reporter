b27e06faf5fd0bc7c2795d9f9ee26a221e9bcab8
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-08-22 13:45:34 +0300

    Improve report test setup #23

--- tests/unit/components/report/setup.js
+++ tests/unit/components/report/setup.js
@@ -1,34 +1,31 @@
-
-function _hasPassed(test) {
+const _hasPassed = test => {
   return test.state === 'passed';
-}
+};
+
+const _hasFailed = test => {
+  return test.state === 'failed';
+};
+
+const _getSuiteDuration = tests => tests.reduce(
+  (acc, {duration}) => acc += duration, 0);
 
 module.exports = function(data) {
   let numberOfPasses = 0,
       numberOfFailures = 0,
       duration = 0,
-      suites = data;
+      suites = [];
 
-  suites.forEach(function(suite) {
-    suite.passes = suite.tests.filter(function(test) {
-      return _hasPassed(test);
-    });
-    numberOfPasses += suite.passes.length;
+  data.forEach(function(item) {
+    const tests = item.tests,
+          passes = tests.filter(_hasPassed),
+          failures = tests.filter(_hasFailed),
+          suite = Object.assign({}, item, {passes, failures});
 
-    suite.failures = suite.tests.filter(function(test) {
-      return !_hasPassed(test);
-    });
-    numberOfFailures += suite.failures.length;
-
-    suite.tests.forEach(function(test) {
-      duration += test.duration;
-    });
+    suites.push(suite);
+    numberOfPasses += passes.length;
+    numberOfFailures += failures.length;
+    duration += _getSuiteDuration(suite.tests);
   });
 
-  return {
-    suites: suites,
-    numberOfPasses: numberOfPasses,
-    numberOfFailures: numberOfFailures,
-    duration: duration
-  };
+  return {suites, numberOfPasses, numberOfFailures, duration};
 };

57b7556e328fbf08fc6d791a153f92e8e6c4732d
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-08-23 15:46:19 +0300

    Add some tests and generate report #29

---
+++ tests/acceptance/test.js
@@ -0,0 +1,90 @@
+'use strict';
+import {expect} from 'chai';
+import chai from 'chai';
+import Mugshot from 'mugshot';
+import chaiMugshot from 'chai-mugshot';
+import webdriverio from 'webdriverio';
+import path from 'path';
+
+const WebdriverIOAdapter = Mugshot.adapters.WebdriverIO;
+const MUGSHOT_OPTIONS = {
+  rootDirectory: './tests/acceptance/visual-tests'
+};
+const BROWSER_OPTIONS = {
+  desiredCapabilities: {
+    browserName: 'PhantomJS'
+  }
+};
+const URL = 'file://' + path.join(__dirname, 'test.html');
+
+describe('Testing the first time', function() {
+  let browser, webdriverioInstance;
+
+  before(function(done) {
+    webdriverioInstance = webdriverio.remote(BROWSER_OPTIONS).init()
+      .url(URL)
+      .then(function() {
+        browser = new WebdriverIOAdapter(webdriverioInstance);
+
+        done();
+      });
+  });
+
+  it('should be ok', function() {
+    const captureItem = {name: 'screen1'},
+          mugshot = new Mugshot(browser, MUGSHOT_OPTIONS),
+          _this = this;
+
+    chai.use(chaiMugshot(mugshot, _this.test.ctx));
+    return expect(captureItem).to.be.identical;
+  });
+
+  it('should be ok too', function() {
+    const captureItem = {name: 'screen2'},
+          mugshot = new Mugshot(browser, MUGSHOT_OPTIONS),
+          _this = this;
+
+    chai.use(chaiMugshot(mugshot, _this.test.ctx));
+    return expect(captureItem).to.be.identical;
+  });
+
+  after(function() {
+    return webdriverioInstance.end();
+  });
+});
+
+describe('Testing the second time', function() {
+  let browser, webdriverioInstance;
+
+  before(function(done) {
+    webdriverioInstance = webdriverio.remote(BROWSER_OPTIONS).init()
+      .url(URL)
+      .then(function() {
+        browser = new WebdriverIOAdapter(webdriverioInstance);
+
+        done();
+      });
+  });
+
+  it('should be ok', function() {
+    const captureItem = {name: 'screen1'},
+          mugshot = new Mugshot(),
+          _this = this;
+
+    chai.use(chaiMugshot(mugshot, _this.test.ctx));
+    return expect(captureItem).to.be.identical;
+  });
+
+  it('should be ok too', function() {
+    const captureItem = {name: 'screen2'},
+          mugshot = new Mugshot(browser, MUGSHOT_OPTIONS),
+          _this = this;
+
+    chai.use(chaiMugshot(mugshot, _this.test.ctx));
+    return expect(captureItem).to.be.identical;
+  });
+
+  after(function() {
+    return webdriverioInstance.end();
+  });
+});

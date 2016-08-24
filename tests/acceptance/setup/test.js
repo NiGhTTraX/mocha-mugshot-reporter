bc7b66d4cad06ac8d05f317e36b5254210b1d8ca
adela-istrate-hs <adela.istrate@hootsuite.com>
2016-08-24 16:52:34 +0300

    Make a test fail from not providing a browser #29

--- tests/acceptance/setup/test.js
+++ tests/acceptance/setup/test.js
@@ -52,28 +52,29 @@ describe('Generate a dummy report for testing', function() {
   });
 
   describe('Second suite', function() {
-    let browser, mugshot, webdriverioInstance;
+    let browser, webdriverioInstance;
 
     before(function(done) {
       webdriverioInstance = webdriverio.remote(BROWSER_OPTIONS).init()
         .url(URL)
         .then(function() {
           browser = new WebdriverIOAdapter(webdriverioInstance);
-          mugshot = new Mugshot(browser, MUGSHOT_OPTIONS);
-
           done();
         });
     });
 
     it('should pass and generate diffs', function() {
       const captureItem = {name: 'screen3'};
+      const captureItem = {name: 'screen3'},
+            mugshot = new Mugshot();
 
       chai.use(chaiMugshot(mugshot, this.test.ctx));
       return expect(captureItem).to.be.identical;
     });
 
     it('should be ok', function() {
-      const captureItem = {name: 'screen4'};
+      const captureItem = {name: 'screen4'},
+            mugshot = new Mugshot(browser, MUGSHOT_OPTIONS);
 
       chai.use(chaiMugshot(mugshot, this.test.ctx));
       return expect(captureItem).to.be.identical;
